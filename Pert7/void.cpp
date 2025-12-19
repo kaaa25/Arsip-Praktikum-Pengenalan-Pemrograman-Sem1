6995773ba63d71dd6f9326ca4d2293ceae2d2263
Siti Zulaika <zsizuuuuu@gmail.com>
2025-12-19 14:40:50 +0700

    Create void.cpp

---
+++ Pert7/void.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void perkenalan(){
+    cout << "Hi, nama saya adalah Siti Zulaika dari kelas A" << endl;
+    cout << "Saya memilihi hobi memasak dan olahraga" << endl;
+    cout << "Saya sedang belajar dasar-dasar permrograman c++" << endl;
+}
+
+int main(){
+    perkenalan();
+    return 0;
+}
