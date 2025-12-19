215745e610aec072f456f9e589270515e85fc4b1
Siti Zulaika <zsizuuuuu@gmail.com>
2025-12-19 11:39:14 +0700

    Create ProgramBiodata.cpp

---
+++ pert2/ProgramBiodata.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+string nama = "Siti Zulaika";
+string NPM = "2517051017";
+
+cout << "Selamat Datang Di Praktikum Pengenalan Pemrograman" << endl;
+cout << "Nama saya adalah " << nama << endl;
+cout << "NPM saya adalah " << NPM << endl << endl;
+cout << "Unila Indah" << endl;
+cout << "Be Strong" << endl;
+}
